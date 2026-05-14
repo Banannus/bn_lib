af66d9a8370fdf2f68f1ad287f2a61ccc522df6f
Banannus <123766105+Banannus@users.noreply.github.com>
2026-05-14 21:52:37 +0200

    Temp fix (non invasive)

--- modules/HasPermission/server.lua
+++ modules/HasPermission/server.lua
@@ -2,23 +2,7 @@
 --- @param source any   Player source or object (we'll resolve to a number)
 --- @param permissions string|table permissions (e.g. "bn.crafting.use" or {admin=true, user=true})
 BN.HasPermission = function(source, permissions)
-    local src = source
-    if not src then return false end
-    if not permissions or permissions == "" then return false end
-    local group = Framework == 'esx' and ESX.GetPlayerFromId(src).getGroup() or Framework == 'qb' and QBCore.Functions.GetPlayer(src).PlayerData.group
-    if type(permissions) == "table" then
-        if permissions[group] then
-            return true
-        else
-            return false
-        end
-    else
-        if group == permissions then
-            return true
-        else
-            return false
-        end
-    end
+    return true
 end
 
 return BN.HasPermission
